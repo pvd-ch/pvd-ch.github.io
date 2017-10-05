98e6778af794dd243f0a83633c6432b56e48ee18
SnugFox <snugfox@users.noreply.github.com>
2017-10-05 21:26:12 +0200

    Initial guidelines table

--- src/views/guidelines.vue
+++ src/views/guidelines.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="view-guidelines">
-    Guidelines
+    <table>
+			<thead>
+				<th v-for="(s, idx) in score" :key="idx">{{ s.name }}</th>
+			</thead>
+			<tr>
+				<td v-for="(s, idx) in score" :key="idx">
+					<ul>
+						<li v-for="(d, dIdx) in s.description" :key="dIdx">{{ d }}</li>
+					</ul>
+				</td>
+			</tr>
+		</table>
   </div>
 </template>
 
