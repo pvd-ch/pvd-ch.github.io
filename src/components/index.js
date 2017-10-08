e4a50016e85923a68d707e57838ec74c3e87b705
SnugFox <snugfox@users.noreply.github.com>
2017-10-08 18:20:33 +0200

    Remove unused components

--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,11 @@
 import Vue from 'vue';
+
+import Button from './button.vue';
 import Header from './header.vue';
-import LevelCard from './level-card.vue';
-import LevelCardGroup from './level-card-group.vue';
-import RubricSection from './rubric-section.vue';
 import Rubric from './rubric.vue';
 import RubricControls from './rubric-controls.vue';
-import Button from './button.vue';
 
+Vue.component(Button.name, Button);
 Vue.component(Header.name, Header);
-Vue.component(LevelCard.name, LevelCard);
-Vue.component(LevelCardGroup.name, LevelCardGroup);
-Vue.component(RubricSection.name, RubricSection);
 Vue.component(Rubric.name, Rubric);
 Vue.component(RubricControls.name, RubricControls);
-Vue.component(Button.name, Button);
