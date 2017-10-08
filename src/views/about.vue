c5e5b988974aede68fbe7dfd45e66843e86f343c
SnugFox <snugfox@users.noreply.github.com>
2017-10-08 19:59:11 +0200

    Create about page

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,6 +1,37 @@
 <template>
   <div class="view-about">
-		about
+		<h1>The Problem</h1>
+		The Swiss energy sector is changing due to new regulations and emerging renewable energy technologies. The industry is experiencing a shift from centralized, large scale power production to decentralized power production in the form of prosumer photovoltaics. The addition of prosumer activity has created new challenges for the existing infrastructure of centralized power production. Existing business models of utilities are currently not equipped to face this shift and utilities must adapt in order to survive in the market.
+		<h1>The Goal</h1>
+		In order to aid utilities during this transition period, our goal is to provide a framework for business adaption in the Swiss energy sector. This will be presented in the forms of a survivability rubric and a best practice guide, in the hopes that utilities can perform a self-assessment and evaluate potential next steps.
+		<h1>Objectives and Methods</h1>
+		Our first objective was to analyze the Swiss energy sector. This was accomplished through background research and utility interviews. Understanding the Swiss energy industry provided us with a fundamental basis of how the sector operates, the existing regulations that surround it, and interactions between stakeholders. Background research from scientific journals, case studies, and regulatory legislation provided valuable and historical information about the energy sector. The primary literature consisted of information pertaining to energy experiments, niche-regime ecosystems, perspectives of utilities, perspectives of consumers, and the Swiss energy transition. We interviewed six companies within the industry and each interview included standardized and company specific questions. Conducting interviews with utility companies gave an in depth perspective of the energy sector and the immediate challenges they are currently facing.
+		<br><br>
+		Our second objective was to determine consumer perspectives. This objective was accomplished through the use of surveys in order to gauge consumer interests on the energy sector and their interests in producing their own energy. This is essential because utilities are now interested in understanding consumer needs due to the possibility of a liberalized market. With the impending challenge of decentralization, the role of consumers has expanded into that of prosumers and has created an additional interaction between the three main stakeholders. Consumers are also major influencers of regulation in Switzerland, which directly affects the direction of the Swiss energy sector.
+		<br><br>
+		Our third objective was to evaluate how utilities adapt to changes in the industry. This objective was accomplished through interviews with utilities in order to further guide our understanding of companies’ business models and their motivations to adapt. We acquired information regarding ongoing projects, experimentation, and initiatives in order to determine the success and effectiveness of certain business strategies. The strategies and adaptations that utilities are integrating into their business models are allowing us to analyze responses to Switzerland’s changing landscape.
+		<h1>Findings</h1>
+		Our background research, interviews, and surveys have led to five key findings. First, we <em>found the business plan of simply selling electricity is no longer sustainable</em>. In the past, utilities have been able to directly produce, distribute, and sell to consumers. Now with the addition of prosumers, the relationship between utility and consumer has changed. All of our interviews agreed that solely selling electricity is no longer sustainable and Swiss utilities are currently in different stages of transitioning out of that business model.
+		<br><br>
+		As an expansion of the first finding <em>we found utilities need to expand their products and services in order to survive</em>. All interviewed utility companies expressed a need to explore additional markets not only to help raise profits, but also to diversify. As one company said, “It is always better to stand on more than two legs.” The new markets include services such as grid projects, telecommunications, efficiency consulting, e-mobility, and PV solutions.
+		<br><br>
+		Along with expanding services, <em>we found utilities are showing increased interest in understanding consumers and their needs</em>. Currently, Swiss utilities lack a full understanding of their customers. Consumer needs have never been a priority due to the partially monopolized market. Decentralization and overcapacity of the grid are causing a shift in the market in which consumers are affecting the direction of the energy sector. In order to prepare for market shifts, utilities are focusing resources to understand consumer needs, create customer-oriented services, and eventually implement those into their business models.
+		<br><br>
+		Switzerland’s government is a major influencer in the energy market and <em>we found complicated regulations make it difficult for companies to operate</em>. The Swiss energy sector is heavily regulated by the Federal Electricity Commission, restricting utilities from freely experimenting. The political cycle changes approximately every five years and the approval process extends beyond this time frame. The approval process includes substantial paperwork, which further deters utilities from investing in new technologies when regulations would require major design changes even before the project is completed. The fear of drastic policy changes inhibits utilities from new investments and developments.
+		<br><br>
+		After distributing consumer surveys, <em>we found consumers value low costs and green energy</em>. In our survey, we asked consumers what the most important factor is when they consume electricity. The two most important factors were price and renewability. Although this poses a challenge for utilities to deliver both factors to consumers, utilities now have a better understanding of what customers value.
+		<h1>Recommendations</h1>
+		Based on our background research, interviews, survey results, and findings, we formulated a list of recommendations for the Smart Grid Plus team at ZHAW and Swiss utilities. These recommendations focus on the importance of the consumer in the changing energy sector and on the need for additional collaboration between academics and utilities. First, <em>we recommend utilities research consumer needs and wants</em>. The role of consumers is becoming more influential in the energy sector, but there is a lack of research on consumers. Therefore, utilities should allocate resources to conduct their own research on consumer needs and wants in order to design customer-oriented services.
+		<br><br>
+		After understanding consumer needs, <em>we recommend that utilities develop strategies for implementing new technologies and integrate customer-oriented services into their existing business models</em>. In order for utilities to smoothly transition with the changing Swiss energy sector, it is essential for them to innovate and adapt their current business models instead of creating new ones. The results from our interviews showed that developing departments dedicated to exploring niche technologies and allocating resources to create strategies is crucial.
+		<br><br>
+		Additionally, <em>we recommend that utilities increase customer outreach in the forms of marketing and education</em>. As utilities plan to add customer-oriented service to their business models, customer outreach is essential for those services to be successful. This will establish a line of communication with consumers that provide many benefits to utilities. Customers may be more responsive to utilities’ services and support new projects and initiatives.
+		<br><br>
+		A rising challenge in the energy sector involves congestion in the grid during peak production hours and <em>we recommend the development of peak shaving regulations</em>. Peak shaving is a strategy for limiting injection into the grid during hours of peak production by restricting the maximum potential of prosumer installations. With current regulations, utilities are required to accept 100% of prosumer power production and the generated power is automatically accepted into the grid. Utilities must expand grid capacity in order to support the extra load from prosumer contributions. Peak shaving of prosumer power production could potentially mitigate the burden of grid expansions.
+ 		<br><br>
+		Finally, <em>we recommend that ZHAW conducts additional research into negative and positive effects of departmental company structures in the Swiss energy sector</em>. Many utility companies are comprised of multiple independent departments that do not collaborate effectively. This leads to gaps in communications and can result in the repetition of work and the loss of valuable time and money. Although our interviews revealed both positive and negative effects of the departmental company structure, it is unclear whether compartmentalization is beneficial or detrimental overall.
+		<h1>Conclusion</h1>
+		By analyzing the current changes in the Swiss energy sector, we have provided utilities with recommendations to help contribute to their survivability in the market. A rubric for grading the survivability of Swiss electric utilities is included in this paper as a self-assessment and scores each utility based on the four pillars of a business model. It provides a list of criteria that each utility should strive for in order to limit potential risk in the market.
   </div>
 </template>
 
@@ -11,5 +42,15 @@ export default {
 </script>
 
 <style scoped>
+h1 {
+	margin-top: 16px;
+	margin-bottom: 4px;
+	font-size: 2em;
+	border-bottom: 2px solid black;
+	text-align: center;
+}
 
+em {
+	font-weight: bold;
+}
 </style>
